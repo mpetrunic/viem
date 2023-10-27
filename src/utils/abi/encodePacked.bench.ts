ccf413f2c013cfeb7ece9329b75e888dbb2c4f23
Marin Petrunic <marin.petrunic@gmail.com>
2023-10-27 17:20:01 +0200

    chore: add web3js to abi benchmarks
    Signed-off-by: Marin Petrunic <marin.petrunic@gmail.com>

--- src/utils/abi/encodePacked.bench.ts
+++ src/utils/abi/encodePacked.bench.ts
@@ -1,6 +1,7 @@
 import { utils as ethersV5Utils } from 'ethers'
 import { solidityPacked } from 'ethers@6'
 import { bench, describe } from 'vitest'
+import { encodePacked as web3EncodePacked } from 'web3-utils'
 
 import { address } from '../../_test/constants.js'
 
@@ -25,6 +26,14 @@ describe('Encode Packed ABI', () => {
     solidityPacked(
       ['address', 'string', 'bytes4[]'],
       [address.vitalik, 'hello world', ['0xdeadbeef', '0xcafebabe']],
+    )
+  })
+
+  bench('web3@4: `solidityPacked`', () => {
+    web3EncodePacked(
+      { type: 'address', value: address.vitalik },
+      { type: 'string', value: 'hello world' },
+      { type: 'bytes4[]', value: ['0xdeadbeef', '0xcafebabe'] },
     )
   })
 })
